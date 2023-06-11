27d9f96e8f2139c38484e7436dd2213b3d0d3ce5
AnyutaAntonovich <antonovich_am@bk.ru>
2023-06-11 16:16:00 +0600

    Add task 4

---
+++ Task4.py
@@ -0,0 +1,17 @@
+# Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек, если разрешается 
+# сделать один разлом по прямой между дольками (то есть разломить шоколадку на два прямоугольника).
+
+# *Пример:*
+
+# 3 2 4 -> yes
+# 3 2 1 -> no
+
+n = int(input('Input the length of the chocolate bar: '))
+m = int(input('Input the width of the chocolate bar: '))
+k = int(input('How many slices do you want to break off? '))
+
+if k % m == 0 or k % n == 0:
+    print('Yes')
+    breakpoint
+else:
+    print('No')
